f48d4650d8613ceaf5c29c1836ec9780a9d6b21a
socrates018 <socratessuciu8@gmail.com>
2025-06-11 00:34:32 +0300

    finish (initial) NAT logic

--- homework/ryu-nat.py
+++ homework/ryu-nat.py
@@ -21,6 +21,7 @@ Two OpenFlow 1.0 L3 Static Routers and two OpenFlow 1.0 L2 learning switches.
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 from ryu.lib.mac import haddr_to_bin
@@ -30,6 +31,7 @@ from ryu.lib.packet import arp
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import udp
+import random
 
 # Router and host interface IPs and MACs
 # Router 1: left (192.168.1.1, 00:00:00:00:01:01)
@@ -69,7 +71,8 @@ ROUTING_TABLE = {
         H1_IP: 1,
         H2_IP: 1,
         H3_IP: 2,
-        H4_IP: 2
+        H4_IP: 2,
+        H5_IP: 1
     }
 }
 
@@ -87,22 +90,47 @@ ARP_TABLE = {
 }
 
 ROUTER1_SUBNET = "192.168.1.0"
-ROUTER1_SUBNET_MASK = 24
 ROUTER2_SUBNET = "192.168.2.0"
-ROUTER2_SUBNET_MASK = 24
+ROUTER1_TOP_SUBNET = "200.0.0.0"
+
+nat = {}
+reverse_nat = {}
+next_port = 12345
+
 
 class SimpleSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
+    
+    
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        # NAT table: maps (internal_ip, internal_port) -> (external_ip, external_port)
-        self.nat_table = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        dpid = datapath.id
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        #proactive flow for router 2
+        if dpid == 0x1B:
+            match = datapath.ofproto_parser.OFPMatch(
+                        dl_type=ether_types.ETH_TYPE_IP,
+                        nw_proto=17,  # UDP protocol number
+                        nw_dst=ROUTER1_TOP_SUBNET,
+                        nw_dst_mask=24
+                    )
+            actions = [
+                    datapath.ofproto_parser.OFPActionSetDlSrc(ROUTER2_LEFT_MAC),
+                    datapath.ofproto_parser.OFPActionSetDlDst(ROUTER1_RIGHT_MAC),
+                    datapath.ofproto_parser.OFPActionOutput(1)
+                ]
+            self.add_flow(datapath, match, actions)
 
     def add_flow(self, datapath, match, actions):
         ofproto = datapath.ofproto
-
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, match=match, cookie=0,
             command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
@@ -120,6 +148,25 @@ class SimpleSwitch(app_manager.RyuApp):
             data=pkt.data
         )
         datapath.send_msg(out)
+
+    def add_nat(self, internal_ip, internal_port):
+        global next_port
+        key = (internal_ip, internal_port)
+        if key in nat:
+            return nat[key]
+        external = (ROUTER1_TOP_IP, next_port)
+        nat[key] = external
+        reverse_nat[external] = key
+        next_port += 1
+        # nat.get((ip, port))
+        # reverse_nat.get((ip, port))
+        return external
+
+    def lookup_external(self, ROUTER1_TOP_IP, external_port):
+        key = (ROUTER1_TOP_IP, external_port)
+        if key in reverse_nat:
+            return reverse_nat[key]
+        return None
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -153,12 +200,45 @@ class SimpleSwitch(app_manager.RyuApp):
                     self.arp_reply(datapath, eth, arp_pkt, msg.in_port)
                 return
             elif ethertype == ether_types.ETH_TYPE_IP:
+                if ip_pkt.dst == ROUTER1_TOP_IP and msg.in_port == 3:
+                    if not udp_pkt:
+                        self.logger.info("[0x1A] Non-UDP packet, dropping")
+                        return
+                    # Reverse NAT: use lookup_external to find mapping
+                    internal = self.lookup_external(ip_pkt.dst, udp_pkt.dst_port)
+                    if internal:
+                        internal_ip, internal_port = internal
+                        self.logger.info(f"Reverse NAT: ({ip_pkt.dst}, {udp_pkt.dst_port}) -> ({internal_ip}, {internal_port})")
+                        out_port = ROUTING_TABLE[dpid][internal_ip]
+                        if out_port == 1:
+                            router_mac = ROUTER1_RIGHT_MAC
+                        elif out_port == 2:
+                            router_mac = ROUTER1_LEFT_MAC
+                     
+                        dst_mac = ARP_TABLE[internal_ip]
+                        match = datapath.ofproto_parser.OFPMatch(
+                            dl_type=ether_types.ETH_TYPE_IP,
+                            nw_proto=17,
+                            nw_src=ip_pkt.src,
+                            nw_dst=ip_pkt.dst, #or ROUTER1_TOP_IP,
+                            tp_dst=udp_pkt.dst_port
+                        )
+                        actions = [
+                            datapath.ofproto_parser.OFPActionSetDlSrc(router_mac),
+                            datapath.ofproto_parser.OFPActionSetDlDst(dst_mac),
+                            datapath.ofproto_parser.OFPActionSetNwDst(internal_ip),
+                            datapath.ofproto_parser.OFPActionSetTpDst(internal_port),
+                            datapath.ofproto_parser.OFPActionOutput(out_port)
+                        ]
+                        self.modify_and_send_ip_packet(datapath, msg.in_port, pkt, actions, out_port)
+                        self.add_flow(datapath, match, actions)
+                        return
+                    else:
+                        self.logger.info(f"No reverse NAT mapping found for {(ip_pkt.dst, udp_pkt.dst_port)}, dropping packet")
+                        return
+                    
                 out_port = ROUTING_TABLE[dpid][ip_pkt.dst]
 
-                # if out_port == ROUTING_TABLE[dpid][ip_pkt.src]:
-                #     self.logger.info("[0x1A] Same subnet, drop")
-                #     
-                
                 if out_port == 3:
                     if not udp_pkt:
                         self.logger.info("[0x1A] Non-UDP packet on port 3, dropping")
@@ -168,31 +248,35 @@ class SimpleSwitch(app_manager.RyuApp):
                     match = datapath.ofproto_parser.OFPMatch(
                         dl_type=ether_types.ETH_TYPE_IP,
                         nw_proto=17,  # UDP protocol number
-                        nw_dst=200.0.0.0,
-                        nw_dst_mask=24
+                        nw_dst=ROUTER1_TOP_SUBNET,
+                        nw_dst_mask=24,
+                        nw_src=ip_pkt.src,
                     )
-                    
-                    # Save NAT mapping: (src_ip, src_port) -> (ROUTER1_TOP_IP, src_port)
-                    nat_key = (ip_pkt.src, udp_pkt.src_port)
-                    nat_val = (ROUTER1_TOP_IP, udp_pkt.src_port)
-                    self.nat_table[nat_key] = nat_val
-                    self.logger.info(f"NAT table updated: {nat_key} -> {nat_val}")
+                    # Use add_nat to get (ROUTER1_TOP_IP, external_port)
+                    ROUTER1_TOP_IP, external_port = self.add_nat(ip_pkt.src, udp_pkt.src_port)
+                    self.logger.info(f"NAT table updated: ({ip_pkt.src}, {udp_pkt.src_port}) -> ({ROUTER1_TOP_IP}, {external_port})")
 
                     actions = [
                         datapath.ofproto_parser.OFPActionSetDlSrc(router_mac),
                         datapath.ofproto_parser.OFPActionSetDlDst(dst_mac),
-                        datapath.ofproto_parser.OFPActionOutput(out_port),
                         datapath.ofproto_parser.OFPActionSetNwSrc(ROUTER1_TOP_IP),
-                        datapath.ofproto_parser.OFPActionSetTpSrc(12345)
+                        datapath.ofproto_parser.OFPActionSetTpSrc(external_port),
+                        datapath.ofproto_parser.OFPActionOutput(out_port)
                     ]
+
                 elif(out_port == 1):
                     router_mac = ROUTER1_RIGHT_MAC
                     dst_mac = ROUTER2_LEFT_MAC
                     match = datapath.ofproto_parser.OFPMatch(
                         dl_type=ether_types.ETH_TYPE_IP,
                         nw_dst=ROUTER2_SUBNET,
-                        nw_dst_mask=ROUTER2_SUBNET_MASK
+                        nw_dst_mask=24
                     )
+                    actions = [
+                        datapath.ofproto_parser.OFPActionSetDlSrc(router_mac),
+                        datapath.ofproto_parser.OFPActionSetDlDst(dst_mac),
+                        datapath.ofproto_parser.OFPActionOutput(out_port)
+                    ]
                 else:
                     router_mac = ROUTER1_LEFT_MAC
                     dst_mac = ARP_TABLE[ip_pkt.dst]
@@ -200,18 +284,16 @@ class SimpleSwitch(app_manager.RyuApp):
                         dl_type=ether_types.ETH_TYPE_IP,
                         nw_dst=ip_pkt.dst
                     )
-
+                    actions = [
+                        datapath.ofproto_parser.OFPActionSetDlSrc(router_mac),
+                        datapath.ofproto_parser.OFPActionSetDlDst(dst_mac),
+                        datapath.ofproto_parser.OFPActionOutput(out_port)
+                    ]
 
                 self.logger.info(f"[DPID {hex(dpid)}] {msg.in_port} -> {out_port}, "
                                  f"{router_mac} -> {dst_mac}, "
                                  f"{ip_pkt.src} -> {ip_pkt.dst}")
 
-                actions = [
-                    datapath.ofproto_parser.OFPActionSetDlSrc(router_mac),
-                    datapath.ofproto_parser.OFPActionSetDlDst(dst_mac),
-                    datapath.ofproto_parser.OFPActionOutput(out_port)
-                ]
-                
                 self.modify_and_send_ip_packet(datapath, msg.in_port, pkt, actions, out_port)
                 self.add_flow(datapath, match, actions)
                 return
@@ -223,17 +305,13 @@ class SimpleSwitch(app_manager.RyuApp):
             elif ethertype == ether_types.ETH_TYPE_IP:
                 out_port = ROUTING_TABLE[dpid][ip_pkt.dst]
 
-                # if out_port == ROUTING_TABLE[dpid][ip_pkt.src]:
-                #     self.logger.info("[0x1B] Same subnet, drop")
-                #     return
-
                 if(out_port == 1):
                     router_mac = ROUTER2_LEFT_MAC
                     dst_mac = ROUTER1_RIGHT_MAC
                     match = datapath.ofproto_parser.OFPMatch(
                         dl_type=ether_types.ETH_TYPE_IP,
                         nw_dst=ROUTER1_SUBNET,
-                        nw_dst_mask=ROUTER1_SUBNET_MASK
+                        nw_dst_mask=24
                     )
                 else:
                     router_mac = ROUTER2_RIGHT_MAC
@@ -330,3 +408,4 @@ class SimpleSwitch(app_manager.RyuApp):
             self.logger.info("port modified %s", port_no)
         else:
             self.logger.info("Illegal port state %s %s", port_no, reason)
+
